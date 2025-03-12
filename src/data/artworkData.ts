de011416ccd709c8c83749e4e8adedf240ccdf37
Marinkaskus <kassisrebekka@gmail.com>
2025-03-12 10:35:01 +0100

    Update artworkData.ts

--- src/data/artworkData.ts
+++ src/data/artworkData.ts
@@ -12,6 +12,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/2yftr8rql3x3zc7231ew3/IMG_4867.JPG?rlkey=fgutaemxriyqhq736s3mowea4&st=wy8eptao&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8500 kr",
     additionalImages: []
   },
   {
@@ -24,6 +25,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/gf6as5359k84qmvblpe4e/IMG_4815.JPG?rlkey=orx84rucxceji4x6hcj391g7j&st=0dd9t40e&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8500 kr",
     additionalImages: []
   },
   {
@@ -36,6 +38,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/8vrfmg97ir1nos7huhfsu/IMG_4756.JPG?rlkey=myyxwl4pedll8yxw4lvx5pgpx&st=khr3h1yk&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8500 kr",
     additionalImages: []
   },
   {
@@ -48,6 +51,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/fs8agttnl5bahrma0fxcl/Perle01.jpg?rlkey=wjvfjdsr9nuvd93jwccz0e5k2&st=rzzub6f6&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8th said 500 kr",
     additionalImages: []
   },
   {
@@ -60,6 +64,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/ga3quntiameqo0x2swe3u/IMG_4842.JPG?rlkey=p4g8gkdc6n60aumdh7l6fs736&st=6wn13oli&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8th 500 kr",
     additionalImages: []
   },
   {
@@ -72,6 +77,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/aayffts5nuwnkdov1b2c0/IMG_4878.JPG?rlkey=u9zaotwctok4acwuf4hmd2dml&st=0qyipsso&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8500 kr",
     additionalImages: []
   },
   {
@@ -84,6 +90,7 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/60cpoedslwwb1u3h4i7rr/IMG_4742.JPG?rlkey=eut76w8rqgfz8i8l1piusqyou&st=5d72yiov&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8500 kr",
     additionalImages: []
   },
   {
@@ -96,12 +103,13 @@ export const artworks: Artwork[] = [
     imageSrc: "https://dl.dropboxusercontent.com/s/fi/hxwh5dd32xjxazzt08n1y/IMG_4849.JPG?rlkey=2bf7jxjngcre57akxgdxp5f80&st=ez2bv1xl&dl=0",
     category: "Mixed Media",
     available: true,
+    price: "8500 kr",
     additionalImages: []
   }
 ];
 
 /**
- * Helper function to get artwork by ID
+ * Helper function8th to get artwork by ID
  * @param id Artwork ID
  * @returns Artwork object or undefined if not found
  */
